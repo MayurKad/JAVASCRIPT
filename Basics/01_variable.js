4be69576643442f6384df491bfce1473ca701264
MayurKad <mayurkad07@gmail.com>
2023-11-09 15:15:09 +0530

    variables defind

---
+++ Basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountID = 124567
+let accountEmail = "baki02@gmail.com"
+var accountPassword = "12567890"
+accountcity = "mumbai"
+let accountstate = ""
+
+// accountID = 4 // not allowd to change
+accountEmail = "mkmk@gmail.com"
+accountPassword = "90909090"
+accountcity = "navi mumbai"
+
+/*
+Prefer to note use the var
+bcz of issue in block scope and funtional scope
+*/
+console.log(accountID);
+console.table([accountEmail,accountID,accountPassword,accountcity, accountstate])
