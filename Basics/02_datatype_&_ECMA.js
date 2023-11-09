ee3db221ab356c904ffacd51aa12147cf86c9331
MayurKad <mayurkad07@gmail.com>
2023-11-09 15:18:05 +0530

    Datatype & ECMA

---
+++ Basics/02_datatype_&_ECMA.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version 
+
+// aleart(2 + 1) // we are using node.js not browser
+
+ console.log(2 + 
+    1
+    ); // code readability should be high  
+console.log("Baki");
+
+let name = "Baki"
+let age = 18
+let isLogIn = false
+let stand;
+
+// number ==> 2 to power of 53
+// bigint
+// string ==> ""
+// Boolean ==> true/false
+// null ==> standalone value
+// undefined ==>
+// symbol ==> unique 
+
+// object 
+
+console.log(typeof null); // output of null is object
+console.log(typeof undefined); // output of undefined id undefined
