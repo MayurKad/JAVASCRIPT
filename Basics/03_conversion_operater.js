6198ceb943696a24313c24340982f3feb182f7ca
MayurKad <mayurkad07@gmail.com>
2023-11-10 10:57:13 +0530

    conversion of datatype

---
+++ Basics/03_conversion_operater.js
@@ -0,0 +1,25 @@
+let score = "22abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggin = 1
+let booleanisLoggin = Boolean(isLoggin)
+console.log(booleanisLoggin);
+
+// 1 => true; 0 => false
+// "" => false
+// "Baki" => true 
+
+let SomeNumber = 22
+
+let StringNumber = String(SomeNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
