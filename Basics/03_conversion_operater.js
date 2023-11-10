e040e736cea6e8557f4143fb76f09bef2e574693
MayurKad <mayurkad07@gmail.com>
2023-11-10 11:30:21 +0530

    Operaters

--- Basics/03_conversion_operater.js
+++ Basics/03_conversion_operater.js
@@ -23,3 +23,42 @@ let SomeNumber = 22
 let StringNumber = String(SomeNumber)
 console.log(StringNumber);
 console.log(typeof StringNumber);
+
+// ***************Operations*********************
+
+let value = 22
+
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+ let str1 = "Baki"
+ let str2 = " Hanma"
+
+ let str3 = str1 + str2 
+ console.log(str3);
+
+ console.log("1" + 2);
+ console.log(1  + "2");
+ console.log("1" + 2 + 2);
+ console.log(1 + 2 + "2");
+ console.log((2 * 4 )+ 5 % 2 );
+
+ console.log(+true);
+ console.log(+"");
+
+ let num1, num2, num3
+
+ num1 = num2 = num3 = 2+2
+
+ let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// prefix and profix study from mdm or js website 
