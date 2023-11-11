db499899b5d0e38bcf8c9a37dd001ab1bc6d1119
MayurKad <mayurkad07@gmail.com>
2023-11-11 12:10:19 +0530

    Summry of datatypes

---
+++ Basics/datatype_summry.js
@@ -0,0 +1,42 @@
+ // Primitive have 7 types
+
+ // 7 types : string, Number, Boolean, Null, Undefined, Symbol, Bigint
+
+ //Refreance (Non primitive)
+
+ //Array, Obajcets, Functions 
+
+ //JavaScript is a dynamically typed languageJavaScript is a dynamically typed language.
+
+ let num = 100
+ let Num = 100.4
+
+ let Boolean = false
+ let Null = null
+ let Undefined;
+
+ const id = Symbol('124')
+ const anotherId = Symbol('124')
+
+ console.log(id == anotherId);
+
+ let BigNum = 1245678899465464600n
+
+ // Array
+
+ let anime = ["baki", "hanma", "jack", "oliver"]
+
+ // object
+
+let student = {
+    name: "Baki",
+    age: "19",
+}
+
+//Functions
+
+const Functions = function(){
+    console.log("Helo world");
+}
+
+console.log(typeof anime);
