73cfef2d15612e09e4f38b0f550a9edd9d27765e
MayurKad <mayurkad07@gmail.com>
2023-11-14 19:44:56 +0530

    date and time half

---
+++ Basics/07_date_&_time.js
@@ -0,0 +1,12 @@
+// Dates
+
+let Mydate = new Date()
+console.log(Mydate.toDateString());
+console.log(Mydate.toISOString());
+console.log(Mydate.toJSON());
+console.log(Mydate.toLocaleDateString());
+console.log(Mydate.toLocaleTimeString());
+console.log(typeof Mydate);
+
+let MynewDate = new Date(2021, 5, 28)
+console.log(MynewDate.toLocaleTimeString());
